fix image feature dimensions; mean in OT loss; gradient clipping; optionally non-trainable mm temperature; fix lr

models/LICO_loss.py
@@ -15,7 +15,7 @@ class LICOLoss(nn.Module):
             'mean': the sum of the output will be divided by the number of
             elements in the output, 'sum': the output will be summed. Default: 'none'
     """
-    def __init__(self, alpha=10., beta=1., reduction='none'):
+    def __init__(self, alpha=10., beta=1., reduction='none', train_mm_temperature=True):
         super(LICOLoss, self).__init__()
 
         self.alpha = alpha
@@ -23,7 +23,9 @@ class LICOLoss(nn.Module):
         self.reduction = reduction
 
         self.ce_loss = nn.CrossEntropyLoss(reduction='none')
-        self.mm_loss = ManifoldMatchingLoss(reduction='none', implementation='ours')
+        self.mm_loss = ManifoldMatchingLoss(reduction='none',
+                                            implementation='ours',
+                                            train_temperature=train_mm_temperature)
         self.ot_loss = SinkhornDistance(eps=1e-4, max_iter=100, reduction='none')
 
     def forward(self, predictions, targets, features_visual, features_text):

models/LICO_model.py
@@ -9,7 +9,7 @@ from training_utils import accuracy
 
 
 class LICOModel(pl.LightningModule):
-    def __init__(self, image_model, target_names: torch.tensor, alpha, beta):
+    def __init__(self, image_model, target_names: torch.tensor, alpha, beta, train_mm_temp):
         """
         :param image_model: the image model to use for feature extraction nad classification
             has to implement features_forward() method that gives features and logits
@@ -32,7 +32,7 @@ class LICOModel(pl.LightningModule):
             nn.Linear(768, self.image_model.get_feature_dim(), dtype=torch.float16)
         )  # h
 
-        self.criterion = LICOLoss(alpha, beta, reduction='mean')
+        self.criterion = LICOLoss(alpha, beta, reduction='mean', train_mm_temperature=train_mm_temp)
         
         # self.criterion = LICOLoss(reduction='mean')
         # hyperparameter
@@ -52,7 +52,7 @@ class LICOModel(pl.LightningModule):
         :param batch: tuple of (images, target)
         """
         images, target = batch
-        img_features, img_logits = self.image_model.features_forward(images)
+        img_logits, img_features = self.image_model.features_forward(images)
         # full_prompt = torch.cat([
         #     torch.broadcast_to(self.get_learnable_prompts(), (target.shape[0], self.M, self.tokenizer_dim)),
         #     self.target_names[target]
@@ -64,9 +64,7 @@ class LICOModel(pl.LightningModule):
         # Apply projection MLP (independently on each token embedding)
         projected_text_features = self.projection_mlp(text_features)
 
-        # todo: i have no idea what the num_channels and prompt_size are for the loss
-        # so for now its 1
-        img_features = img_features.view(img_features.shape[0], 1, img_features.shape[1])
+        # 1 here is number of tokens in the prompt
         projected_text_features = projected_text_features.view(projected_text_features.shape[0], 1, projected_text_features.shape[1])
         # if torch.isnan(img_features).any():
             # raise ValueError("Image features contain NaN values.")
@@ -84,14 +82,13 @@ class LICOModel(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         images, target = batch
-        img_features, img_logits = self.image_model.features_forward(images)
+        img_logits, img_features = self.image_model.features_forward(images)
         full_prompt = self.target_names[target].view(target.shape[0], self.clip_tokenizer_dim)
         with torch.no_grad():
             text_features = self.text_model.encode_text(full_prompt)
 
         projected_text_features = self.projection_mlp(text_features)
 
-        img_features = img_features.view(img_features.shape[0], 1, img_features.shape[1])
         projected_text_features = projected_text_features.view(projected_text_features.shape[0], 1,
                                                                projected_text_features.shape[1])
         loss, mm_part, ot_part = self.criterion(img_logits, target, img_features, projected_text_features)

models/image_model.py
@@ -2,36 +2,46 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torch import Tensor
 from torchmetrics.classification import MulticlassAccuracy
 
 from training_utils import accuracy
+
+
+def features_forward(model, x):
+    x = model.conv1(x)
+    x = model.bn1(x)
+    x = model.relu(x)
+    x = model.maxpool(x)
+
+    x = model.layer1(x)
+    x = model.layer2(x)
+    x = model.layer3(x)
+    features = model.layer4(x)
+
+    x = model.avgpool(features)
+    x = torch.flatten(x, 1)
+    x = model.fc(x)
+
+    return x, features
 
 
 class ImageClassificationModel(pl.LightningModule):
     def __init__(self, pretrained, arch, lr, momentum, weight_decay, num_classes):
         super().__init__()
 
-        class Identity(nn.Module):
-            def __init__(self):
-                super(Identity, self).__init__()
-
-            def forward(self, x):
-                return x
-
         print(f"=> {'using pre-trained' if pretrained else 'creating'} model {arch}")
         self._model = models.__dict__[arch](pretrained=pretrained)
 
-        # Modify the final layer based on the architecture
-        # we will use the penultimate layer's outputs for feature extraction
-        self._model.fc = Identity()
-
+        assert 'resnet' in arch, 'Only resnet architectures are supported'
         # and this one will be for classification
         if arch == 'resnet18':
-            self.feature_dim = 512
+            self.num_channels = 512
         elif arch == 'resnet50':
-            self.feature_dim = 2048
+            self.num_channels = 2048
+        self.feature_dim = 49
 
-        self._actual_fc = nn.Linear(self.feature_dim, num_classes)
+        self._model_fc = nn.Linear(self.num_channels, num_classes)
 
         self.criterion = nn.CrossEntropyLoss()
         self.lr = lr
@@ -51,11 +61,8 @@ class ImageClassificationModel(pl.LightningModule):
         """
         Returns the features of the penultimate layer and the logits
         """
-        features = self._model(x)
-        # not detaching features because we need to compute the CE loss on them through logits
-        # the MM and OT loss will also affect features, bypassing the logits
-        logits = self._actual_fc(features)
-        return features, logits
+        logits, features = features_forward(self._model, x)
+        return logits, features.view(logits.shape[0], self.num_channels, self.feature_dim)
 
     def training_step(self, batch, batch_idx):
         images, target = batch

models/mm_loss.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 
 class ManifoldMatchingLoss(nn.Module):
-    def __init__(self, distance_type='euc', reduction='none', implementation='ours'):
+    def __init__(self, distance_type='euc', reduction='none', implementation='ours', train_temperature=True):
         """Manifold matching loss from LICO
         """
         super(ManifoldMatchingLoss, self).__init__()
@@ -14,7 +14,10 @@ class ManifoldMatchingLoss(nn.Module):
         self.distance_type = distance_type
         self.implementation = implementation
         # Trainable temperature
-        self.temperature = nn.Parameter(torch.tensor(1.0, dtype=torch.float16))
+        if train_temperature:
+            self.temperature = nn.Parameter(torch.tensor(2.5, dtype=torch.float16))
+        else:
+            self.temperature = torch.tensor(2.5, dtype=torch.float16)
     
     def create_adjacent_matrix_ours(self, feats):
         """Create adjacent matrix from a matrix of features

models/sinkhorn_distance.py
@@ -68,7 +68,7 @@ class SinkhornDistance(nn.Module):
         # Transport plan pi = diag(a)*K*diag(b)
         pi = torch.exp(self.M(C, U, V))
         # Sinkhorn distance
-        cost = torch.sum(pi * C, dim=(-2, -1))
+        cost = torch.mean(pi * C, dim=(-2, -1))
 
         if self.reduction == 'mean':
             cost = cost.mean()