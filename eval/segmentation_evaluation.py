76ca77d784bdc03b2c9abb6e2ef6025949be51fb
Konrad Szewczyk <konszewczyk@outlook.com>
2024-01-26 21:42:43 +0100

    TorchCAM + segmentation pointing game

--- eval/segmentation_evaluation.py
+++ eval/segmentation_evaluation.py
@@ -1,9 +1,10 @@
 import numpy as np
-from torch.utils.data.dataset import T_co
+import torchvision.transforms.functional
 from tqdm import tqdm
 import os
 
 import pandas as pd
+import cv2
 
 import torch
 import torch.nn as nn
@@ -13,15 +14,23 @@ import torchvision.datasets as datasets
 import torchvision.models as models
 import torch.nn.functional as F
 
+from torchcam.methods import GradCAM, GradCAMpp, ScoreCAM
+from torchvision.transforms.functional import normalize, resize, to_pil_image
+
 from eval.utils import *
 from models.image_model import ImageClassificationModel
 from eval.evaluation import CausalMetric, auc, gkern
-from training_utils import TEXT_CLASSES
-from eval.cam import GradCAM
+from training_utils import TEXT_CLASSES, DATASETS_TO_CLASSES
+# from eval.cam import GradCAM, EigenGradCAM, ScoreCAM, GradCAMPlusPlus
+from torchcam.utils import overlay_mask
 from datasets.imagefolder_cgc_ssl import ImageFolder as CGCImageFolder
 import argparse
 
 parser = argparse.ArgumentParser(description="PyTorch Equivariance Evaluation")
+
+parser.add_argument(
+    "--cam", dest="cam", default='grad-cam', type=str, help="explainability method to use"
+)
 parser.add_argument(
     "--pretrained", dest="pretrained", action="store_true", help="use pre-trained model"
 )
@@ -32,12 +41,17 @@ parser.add_argument(
     "--arch", dest="arch", default='resnet18', type=str, help="model architecture"
 )
 parser.add_argument(
+    "--training-method", dest="training_method", default="baseline", type=str, help="training scheme"
+)
+parser.add_argument(
+    "--dataset", dest="dataset", default='imagenet-s50', type=str, help="dataset to evaluate on"
+)
+parser.add_argument(
     "--img_data", dest="img_data", default='data/ImageNetS50/val', type=str, help="path to images"
 )
 parser.add_argument(
     "--seg_data", dest="seg_data", default='data/ImageNetS50/validation-segmentation', type=str, help="path to segmentation"
 )
-
 
 
 class ImageFolderWithSegmentation(datasets.ImageFolder):
@@ -57,6 +71,7 @@ class ImageFolderWithSegmentation(datasets.ImageFolder):
         seg = Image.open(seg_name)
         seg = self.seg_transforms(seg).to(torch.int32)
         seg = seg[0] + 256 * seg[1] + 256**2 * seg[2]
+        seg = (seg != 0)
 
         return (img, label, seg)
 
@@ -65,21 +80,20 @@ def main():
     args = parser.parse_args()
 
     cudnn.benchmark = True
+    #args.pretrained = True
 
     if args.pretrained:
-        net = ImageClassificationModel(
-            arch=args.arch,
-            num_classes=50,
-            pretrained=True,
-            lr=None,
-            momentum=None,
-            weight_decay=None,
-            total_steps=None)
-        pass
+        if args.arch == 'resnet18':
+            net = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+        elif args.arch == 'resnet50':
+            net = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+        else:
+            raise NotImplementedError()
+        target_layer = net.layer4[-1]
     else:
         net = ImageClassificationModel(
             arch=args.arch,
-            num_classes=50,
+            num_classes=DATASETS_TO_CLASSES[args.dataset],
             pretrained=False,
             lr=None,
             momentum=None,
@@ -88,12 +102,15 @@ def main():
         if not args.ckpt_path:
             raise Exception("Pretrained is set to False, but not checkpoint path found")
         state_dict = torch.load(args.ckpt_path)["state_dict"]
-
+        if args.training_method == 'lico':
+            state_dict = {k.replace("image_model.", ""): v for k, v in state_dict.items() if "image_model." in k}
         # load params
         net.load_state_dict(state_dict)
 
-    target_layer = net._model.layer4[-1]
-    cam = GradCAM(model=net, target_layer=target_layer, use_cuda=True)
+        target_layer = net._model.layer4[-1]
+
+    net.cuda()
+    cam = GradCAM(model=net, target_layer=target_layer)
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
@@ -102,7 +119,7 @@ def main():
         transforms.Resize(224),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        normalize,
+        #normalize,
     ])
 
     seg_transforms = transforms.Compose([
@@ -112,27 +129,48 @@ def main():
     ])
 
     val_dataset = ImageFolderWithSegmentation(args.img_data, args.seg_data, img_transforms, seg_transforms)
-    val_dataloader = DataLoader(val_dataset, batch_size=128)
+    val_dataloader = DataLoader(val_dataset, batch_size=8)
 
     res_labels = np.array([])
     res_scores = np.array([])
 
     for img, label, seg in val_dataloader:
-        salience = cam(img, label)
+        norm_img = normalize(img).cuda()
+        output = net(norm_img)
+        #salience = cam(norm_img, label)
+        salience = cam(label.tolist(), output)[0]
 
-        seg_label = label + 1
-        seg_label = seg_label.view(-1, 1, 1)
-        seg_binary = seg == seg_label
-        seg_binary = seg_binary.numpy()
+        salience = [to_pil_image(sal.unsqueeze(0)).resize(img.shape[2:], resample=Image.BICUBIC) for sal in salience]
+        salience = np.stack([np.float32(sal) / 255 for sal in salience], axis=0)
+        salience = torch.tensor(salience)
 
-        total_salience = salience.sum(axis=(1, 2), keepdims=True)
-        norm_salience = salience / total_salience
+        total_salience = salience.sum(dim=(1, 2), keepdims=True)
+        norm_salience = salience / (total_salience + 1e-5)
 
-        segmentation_score = norm_salience * seg_binary
-        segmentation_score = segmentation_score.sum(axis=(1, 2))
+        segmentation_score = norm_salience * seg
+        segmentation_score = segmentation_score.sum(dim=(1, 2))
 
-        res_labels = np.concatenate([res_labels, label])
-        res_scores = np.concatenate([res_scores, segmentation_score])
+        res_labels = np.concatenate([res_labels, label.numpy()])
+        res_scores = np.concatenate([res_scores, segmentation_score.numpy()])
+
+        # idx = np.argmax(label > 1)
+
+        # sal_img = cv2.applyColorMap((salience[idx, :, :] * 255).astype(np.uint8), cv2.COLORMAP_TURBO)
+        # weight = cv2.addWeighted(img, 0.5, sal_img, 0.7, 5)
+
+        #sal_img = cv2.applyColorMap((salience[0][idx].detach().cpu().numpy() * 255).astype(np.uint8), cv2.COLORMAP_TURBO)
+        #weight = cv2.addWeighted(img, 0.5, sal_img, 0.7, 5)
+
+        #result = overlay_mask(to_pil_image(img), to_pil_image(salience[0][idx].detach().cpu().squeeze(0), mode='F'), alpha=0.3)
+        #cv2.imshow("saliency", salience[idx, :, :, np.newaxis] * img)
+        # cv2.imshow("saliency", np.array(result)[:, :, ::-1])
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        #cv2.imshow("saliency", salience[idx, :, :, np.newaxis] * img)
+        # cv2.imshow("saliency", weight)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
     results_df = pd.DataFrame({'label': res_labels, 'segmentation_score': res_scores})
     results_df['label'] = results_df['label'].apply(lambda x: TEXT_CLASSES['imagenet-s50'][int(x)])
